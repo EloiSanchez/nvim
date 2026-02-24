870b3d96597a34de89b182a32de33a3a0df69308
Eloi Sanchez <eloisanchez16@gmail.com>
2026-02-24 09:24:34 +0100

    formatting and minor changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,29 +3,29 @@
 --
 -- https://github.com/nvim-treesitter/nvim-treesitter
 local ensure_installed_packages = {
-  'lua',
-  'python',
-  'sql',
-  'vim',
-  'vimdoc',
-  'query',
-  'luadoc',
-  'sql',
-  'markdown',
-  'json',
-  'yaml'
+  "lua",
+  "python",
+  "sql",
+  "vim",
+  "vimdoc",
+  "query",
+  "luadoc",
+  "sql",
+  "markdown",
+  "json",
+  "yaml",
 }
 return {
   {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     lazy = false,
-    build = ':TSUpdate',
+    build = ":TSUpdate",
     config = function()
-      local nvim_ts = require 'nvim-treesitter'
+      local nvim_ts = require("nvim-treesitter")
       nvim_ts.setup()
       nvim_ts.install(ensure_installed_packages)
 
-      vim.api.nvim_create_autocmd('FileType', {
+      vim.api.nvim_create_autocmd("FileType", {
         pattern = ensure_installed_packages,
         callback = function()
           vim.treesitter.start()
@@ -37,6 +37,6 @@ return {
   -- This extension for treesitter allows current config to stick on top of the window
   -- when scrolling down
   {
-    'nvim-treesitter/nvim-treesitter-context',
+    "nvim-treesitter/nvim-treesitter-context",
   },
 }
